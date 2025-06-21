598c89ff93e70dcbf7e435b6d5d870f6b2a1e5f2
umichan0621 <umichan0621@gmail.com>
2025-06-21 16:53:08 +0800

    add common

---
+++ pkg/common/uri.go
@@ -0,0 +1,7 @@
+package common
+
+const (
+	URI_STEAM_API       = "https://api.steampowered.com"
+	URI_STEAM_STROE     = "https://store.steampowered.com"
+	URI_STEAM_COMMUNITY = "https://steamcommunity.com"
+)
